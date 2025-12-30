447046aba2a7a8a365b920562a73e572ca9ee77d
Primak1127 <prym4kdmytr@gmail.com>
2025-12-30 10:46:58 +0100

    Finish website

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,18 @@
 const mobileMenu = document.querySelector(".header__nav");
 const menuBtnOpen = document.querySelector(".menu-btn-open");
 const menuBtnClose = document.querySelector(".menu-btn-close");
+const header = document.querySelector(".header");
 
-const toggleMenu = () => mobileMenu.classList.toggle("is-open");
-const disableScroll = () =>
+const toggleMenu = () => {
+  mobileMenu.classList.toggle("is-open");
+  header.classList.toggle("is-open");
   document.body.classList.toggle("is-scroll-disabled");
+};
+
+// const disableScroll = () =>
 
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
 
-menuBtnOpen.addEventListener("click", disableScroll);
-menuBtnClose.addEventListener("click", disableScroll);
+// menuBtnOpen.addEventListener("click", disableScroll);
+// menuBtnClose.addEventListener("click", disableScroll);
